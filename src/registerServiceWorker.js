7b89f6385df9e31edf7b84fedd0abe74e1e54715
LUHMLLO <luis.jr.melo@gmail.com>
2021-02-10 18:49:50 -0400

    testing pwa notification 3

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -6,7 +6,6 @@ if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
       console.log("App is being served from cache by a service worker.\n" + "For more details, visit https://goo.gl/AFskqB");
-      this.$store.dispatch("launch_notification");
     },
     registered (registration) {
       console.log("Service worker has been registered.");
