1606b3b86627f42222ea6fd7169a77fc8c050cad
LUHMLLO <luis.jr.melo@gmail.com>
2021-02-10 18:17:25 -0400

    blindingly trying to fix the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navbar p-x-16" :class="[theme.primary_background]">
+  <div class="navbar p-x-16 d-none sm:d-flex" :class="[theme.primary_background]">
     <div class="navbar-content">
       <div class="navbar-section">
         <div class="width-100 height-50 offset-l-20">
