29bd8212b1e736fdfbd7c1fdf9d57f6856f16c05
LUHMLLO <luis.jr.melo@gmail.com>
2021-02-10 19:24:45 -0400

    working on notifications + pwa notification

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,27 +1,36 @@
 <template>
   <div class="p-16 d-flex width-300 flex-shrink-1 content-center p-fixed bottom right z-index-11 transition" :class="[notificationStatus ? 'opacity-100' : 'opacity-0 hidden']">
     <div class="w-100 h-100 p-16 depth-6" :class="[theme.radius, theme.accent_background, theme.secondary_color]">
-      <label class="font-xs pb-5 border-b border-grey-ligthen-2 w-100 d-block">Evento actual</label>
+      <label class="font-sm pb-5 border-b border-grey-ligthen-2 w-100 d-block">Notificación</label>
       <div class="d-flex w-100 content-center m-y-5">
         <div class="avatar overflow-hidden m-y-auto ml-0 mr-5" :class="[theme.radius]">
           <img src="@/assets/printo.png" alt="" />
         </div>
-        <h6 class="font-sm m-y-auto ml-0">Blue Moon</h6>
+        <h6 class="font-md m-y-auto ml-0">{{ notificationOwner }}</h6>
       </div>
-      <small class="font-xs d-block">{{ notificationText }}</small>
-      <a href="#" class="font-xs d-inline-block mt-5 pb-2 border-b border-grey-ligthen-2">{{ notificationButtonText }}</a>
+      <small class="font-sm d-block">{{ notificationText }}</small>
+      <a href="#" class="font-sm d-inline-block mt-5 pb-2 border-b border-grey-ligthen-2">{{ notificationButtonText }}</a>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    notificationOwner: {
+      type: String
+    },
+    notificationText: {
+      type: String
+    },
+    notificationButtonText: {
+      type: String
+    }
+  },
   data() {
     return {
       refreshing: false,
-      registration: null,
-      notificationText: "im a notification",
-      notificationButtonText: "Visítanos"
+      registration: null
     };
   },
   created() {
@@ -40,8 +49,9 @@ export default {
     showRefreshUI(e) {
       this.registration = e.detail;
       this.toggleNotification();
-      this.notificationText = "New version available!";
-      this.notificationButtonText = "update now";
+      this.notificationOwner = "¡Nueva versión disponible!";
+      this.notificationText = "Se ha detectado una actualización más reciente, se cargará automáticamente la próxima vez que acceda a la plataforma o puede ejecutar la actualización de inmediato";
+      this.notificationButtonText = "Actualizar ahora";
     },
     refreshApp() {
       // Protect against missing registration.waiting.
