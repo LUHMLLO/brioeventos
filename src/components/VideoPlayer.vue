7ec2d19398fd3bbf618f7c9411649aa09c2ef955
LUHMLLO <luis.jr.melo@gmail.com>
2021-02-11 17:31:49 -0400

    upgrading

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="padding: 56.25% 0 0 0; position: relative">
+  <div class="overflow-hidden" style="padding: 56.25% 0 0 0; position: relative">
     <iframe   
     :src="videoSRC"   
     style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"  
@@ -16,7 +16,7 @@ export default {
   props: {
     videoSRC: {
       type: String,
-      default: "https://player.vimeo.com/video/143418951?autoplay=1&loop=1&color=ffffff&title=0&byline=0&portrait=0"
+      default: "https://player.vimeo.com/video/143418951?autoplay=1&loop=1&color=ffffff&title=1&byline=0&portrait=0"
     }
   },
   mounted(){    
